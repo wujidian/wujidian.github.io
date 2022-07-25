eca82f205f0e34d295bc06633d43b01b32761b62
xlfguang <xlfguang@users.noreply.github.com>
2022-07-25 08:21:25 +0000

    deploy: ca2cc877af481c7c9eb72ccc816ee242e3d1f2d7

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import Router,{useRouter} from "next/router";
+import { ParsedUrlQueryInput } from "querystring";
 export const getLoc = (itemKey: string) => {
   return localStorage.getItem(itemKey);
 };
@@ -6,5 +8,15 @@ export const setLoc = (itemKey: string, itemValue: any) => {
 };
 export const removeLoc = (itemKey: string) => {
   localStorage.removeItem(itemKey);
+};
+
+export const setQuery = (url: string, query: any) => {
+  Router.push({
+    pathname: url,
+    query,
+  });
+  for (const key in query) {
+    setLoc(key, query[key]);
+  }
 };
 
